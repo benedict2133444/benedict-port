4e8f2743fd5dd9e1059eb9afb073233de26a7e1c
unknown <benedict2133444>
2024-10-21 11:40:03 +0800

    modified header

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -33,7 +33,7 @@ const router = createBrowserRouter([
     element: <Skills/>
   },
   {
-    path: 'portfolios',
+    path: 'portfolio',
     element: <Portfolio/>
   },
   {
--- src/components/Header/NavigationList.ts
+++ src/components/Header/NavigationList.ts
@@ -1,7 +1,7 @@
 export const navList = [
   { id: 'about', name: 'About', route: '/about' },
   { id: 'skills', name: 'Skills', route: '/skills' },
-  { id: 'portfolios', name: 'Portfolios', route: '/portfolios' },
+  { id: 'portfolios', name: 'Portfolio', route: '/portfolios' },
   { id: 'experience', name: 'Experience', route: '/experience' },
   { id: 'contact', name: 'Contact', route: '/contact' }
 ]
